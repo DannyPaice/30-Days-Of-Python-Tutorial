06bfeb4fa0af1b97c58c0ed17b83dfbe6cf507c6
Daniel <d.paice@student.uq.edu.au>
2025-11-26 12:46:48 +1000

    exercise 1 complete

---
+++ Day_12-Modules/exercise_level_1.py
@@ -0,0 +1,9 @@
+from random import choice
+from string import ascii_letters, digits
+
+def random_user_id(length=6) -> str:
+    characters = ascii_letters + digits
+    gen = ''.join(choice(characters) for i in range(length))
+    return gen
+
+print(random_user_id())

0fbfc4b7abd060afd967ba660d1b4661358b4a35
Daniel <d.paice@student.uq.edu.au>
2025-11-26 20:43:45 +1000

    finished exercise 1

--- Day_12-Modules/exercise_level_1.py
+++ Day_12-Modules/exercise_level_1.py
@@ -6,3 +6,17 @@ def random_user_id(length=6) -> str:
     gen = ''.join(choice(characters) for i in range(length))
     return gen
 
+
+
+def user_id_gen_by_user() -> str:
+    length1 = int(input("length of id: "))
+    length2 = int(input("number of id(s): "))
+
+    acc = []
+    for i in range(length2):
+        acc.append(random_user_id(length1))
+
+    return acc
+
+print(user_id_gen_by_user())
+    

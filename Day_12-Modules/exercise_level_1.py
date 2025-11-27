cd8dfe49ffd32dcd22270c35ee656f9a657053ff
Daniel <d.paice@student.uq.edu.au>
2025-11-27 18:45:58 +1000

    completed exercise level 2

--- Day_12-Modules/exercise_level_1.py
+++ Day_12-Modules/exercise_level_1.py
@@ -19,4 +19,3 @@ def user_id_gen_by_user() -> str:
     return acc
 
 print(user_id_gen_by_user())
-    
--- Day_12-Modules/exercise_level_2.py
+++ Day_12-Modules/exercise_level_2.py
@@ -4,10 +4,26 @@ Write a function named rgb_color_gen.
 It will generate rgb colors (3 values 0 to 255 each)
 """
 
-from random import randint
+from random import randint, choice
+from string import digits
 
-def rgb_color_gen():
+def rgb_color_gen() -> str:
     r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
     return f"rgb({r}, {g}, {b})"
 
-print(rgb_color_gen())
+def hexa_color_gen() -> str:
+    char = digits + 'abcdef'
+    rand_hexa = ''.join(choice(char) for i in range(6))
+    return f"#{rand_hexa}"
+
+def generate_colors(color_type: str, num: int) -> list[str]:
+    if color_type == 'hexa':
+        #add num * hexa_color_gen() however many times
+        return [hexa_color_gen() for i in range(num)]
+    if color_type == "rgb":
+        #add num * rgb_color_gen() however many times
+        return[rgb_color_gen() for i in range(num)]
+    raise ValueError("color_type must be either hexa or rgb")
+
+
+print(generate_colors('hexa', 2))

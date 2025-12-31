4fe1515fa74b14a1925dcec5747c7e555672153c
Daniel <d.paice@student.uq.edu.au>
2025-12-31 14:17:16 +1000

    added html templetes and app.py

--- Day_26-Modules/python_for_web/app.py
+++ Day_26-Modules/python_for_web/app.py
@@ -1,3 +1,21 @@
 
 from flask import Flask
 import os
+
+app = Flask(__name__)
+
+
+@app.route('/') # decorator create the home route
+def home():
+    return '<h1>Welcome</h1>'
+
+@app.route('/about') 
+def about():
+    return '<h1>about us</h1>'
+
+
+if __name__ == '__main__':
+    # for deployment we use the environ
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
